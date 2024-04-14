0b22e84ad9e4113d6057ec52b361938602aa6248
bidhanphuyal <fuyalbidhan154@gmail.com>
2024-04-14 21:16:09 +0545

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,73 @@
-import logo from './logo.svg';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [options, setOptions] = useState([]);
+  const [to, setTo] = useState("en");
+  const [from, setFrom] = useState("en");
+  const [input, setInput] = useState("");
+  const [output, setOutput] = useState("");
+
+  const translate = () => {
+    //curl -X 'POST' \'https://libretranslate.com/translate' \-H 'accept: application/json' \-H 'Content-Type: application/x-www-form-urlencoded' \-d 'q=hello&source=en&target=es&format=text&api_key=xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx'
+
+
+    const params = new URLSearchParams();
+    params.append('q', input);
+    params.append('source', from);
+    params.append('target', to);
+    params.append('format', 'text');
+    params.append('api_key', 'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx');
+
+    axios.post('https://libretranslate.com/translate', params,
+      {
+        headers: {
+          'accept': 'application/json',
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+      }).then(res => {
+        console.log(res.data)
+        setOutput(res.data.translatedText)
+      });
+  };
+
+
+
+  useEffect(() => {
+    // curl -X 'GET' \'https://libretranslate.com/languages' \-H 'accept: application/json'
+    axios.get('https://libretranslate.com/languages',
+      { headers: { 'accept': 'application/json' } }).then(res => {
+        console.log(res.data);
+        setOptions(res.data);
+      })
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello Translator</h1>
+      <div>
+        From({from}):
+        <select onChange={e => setFrom(e.target.value)}>
+          {options.map(opt => <option key={opt.code} value={opt.code}>{opt.name}</option>)}
+        </select>
+        To({to}):
+        <select onChange={e => setTo(e.target.value)}>
+          {options.map(opt => <option key={opt.code} value={opt.code}>{opt.name}</option>)}
+        </select>
+      </div>
+
+      <div>
+        <textarea cols="50" rows="8" onInput={(e) => setInput(e.target.value)}></textarea>
+      </div>
+
+      <div>
+        <textarea cols="50" rows="8" value={output}></textarea>
+      </div>
+
+      <div>
+        <button onClick={e=>translate()}>Translate</button>
+      </div>
     </div>
   );
 }
